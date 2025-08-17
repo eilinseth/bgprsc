88d4a3f28392960856d2b3d6b9bdfeea1d36c71e
eilinseth <aksun989@gmail.com>
2025-08-17 19:45:17 +0700

    Fix bug ipv6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -61,12 +61,14 @@ function App() {
           const req = await axios.get(`http://localhost:5000/api/asn/${values.asn}`)
           const res =  req.data
           const data = res.data
+          
 
           //misahin ipv4 sama ipv6
-          const ipv4 = data.ipv4_prefixes.map((item : {prefix : string}) => `add address=${item.prefix} list=${values.addresListName}`)
-          const ipv6 = data.ipv6_prefixes.map((item : {prefix : string}) => `add address=${item.prefix} list=${values.addresListName}`)
+          const ipv4 = data.ipv4_prefixes.map((item : {prefix : string}) => `ip firewall address-list add address=${item.prefix} list=${values.addresListName}`)
+          const ipv6 = data.ipv6_prefixes.map((item : {prefix : string}) => `ipv6 firewall address-list add address=${item.prefix} list=${values.addresListName}`)
           //gabungin sintaks firewall di mikrotik nya
-          const rscContent = `/ip firewall address-list\n${ipv4.join("\n")}\n${ipv6.join("\n")}`
+          const rscContent = `${ipv4.join("\n")}\n${ipv6.join("\n")}`
+          console.log(rscContent)
 
           //buat file download
           const blob = new Blob([rscContent],{type:"text/plain"})
@@ -112,7 +114,7 @@ function App() {
     <div id="bg-main" className="  h-screen w-screen  ">
       <div className="bg-white/20 backdrop-blur-sm w-screen h-screen ">
       <section className="px-[6%] pt-20 md:pt-50 ">
-        <h1 className="font-bold text-white drop-shadow-black drop-shadow-xs text-4xl -mt-10 md:-mt-14 ">BGPview ASN to Mikrotik RSC</h1>
+        <h1 className="font-bold text-white drop-shadow-black drop-shadow-xs text-4xl mt-10 md:-mt-14 ">BGPview ASN to Mikrotik RSC</h1>
       </section>
 
         <div className="flex justify-center items-center lg:max-w-[55%] md:max-w-[70%] sm:max-w-[78%] max-w-[85%] sm:mt-4 sm:ml-10 md:mt-2 md:ml-12 lg:ml-20 ml-8 lg:h-40  lg:-mt-2  lg:p-5 mt-5 px-2 py-2 bg-slate-300/40 shadow-lg shadow-white rounded-lg">
@@ -194,13 +196,23 @@ function App() {
 
           </div>
         </section>
-          <div className="h-20 w-screen bg-gradient-to-b from-blue-100 to-slate-300 "></div>
 
-        <section className="min-h-screen w-screen bg-gradient-to-b from-slate-300 to-blue-900 py-15 px-20">
-          <div className="flex justify-center items-center gap-5">
-            <div className="w-[75%]  h-[calc(100vh-80px)]">
-                <h1 className="text-4xl text-center font-bold bg-linear-90 from-[#3B82F6] to-[#9333EA] bg-clip-text text-transparent drop-shadow-white drop-shadow-lg">Tutorial</h1>
+        <section className="min-h-screen w-screen bg-slate-800 py-15 px-20">
+          
+            <div className="w-[75%]  h-[calc(100vh-80px)] mx-auto ">
+                <h1 className="text-4xl text-center font-bold bg-linear-90 from-sky-400 to-purple-500 bg-clip-text text-transparent ">Tutorial</h1>
+            
+
+            <div className="text-white font-semibold flex items-center justify-start mt-20 text-xl">
+              <ol className="list-decimal pl-[2rem] ">
+                <li >You need to know the <b>asn number</b> from <a target="_blank" href="https://bgpview.io/" className="font-bold">bgpview.io</a>. In this tutorial i will use facebook's asn number</li>
+                <li className="mt-4"></li>
+                <li className="mt-4"></li>
+                <li className="mt-4"></li>
+                <li className="mt-4"></li>
+              </ol>
             </div>
+            
 
 
 
