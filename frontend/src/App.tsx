8d10ec462705dac3039539c2ae81beb14436b76c
eilinseth <aksun989@gmail.com>
2025-08-19 14:30:19 +0700

    Tambah footer dan bahasa indonesia

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -115,7 +115,7 @@ function App() {
     <div id="bg-main" className="  h-screen w-screen  ">
       <div className="bg-white/20 backdrop-blur-sm w-screen h-screen ">
       <section className="px-[6%] pt-20 md:pt-50 ">
-        <h1 className="font-bold text-white drop-shadow-black drop-shadow-xs text-4xl mt-10 md:-mt-14 ">BGPview ASN to Mikrotik RSC</h1>
+        <h1 className="font-bold text-white drop-shadow-black drop-shadow-xs text-4xl mt-20 md:-mt-14 ">BGPview ASN to Mikrotik RSC</h1>
       </section>
 
         <div className="flex justify-center items-center lg:max-w-[55%] md:max-w-[70%] sm:max-w-[78%] max-w-[85%] sm:mt-4 sm:ml-10 md:mt-2 md:ml-12 lg:ml-20 ml-8 lg:h-40  lg:-mt-2  lg:p-5 mt-5 px-2 py-2 bg-slate-300/40 shadow-lg shadow-white rounded-lg">
@@ -138,20 +138,20 @@ function App() {
       </div>
     </div>
         <section className="min-h-screen w-screen bg-slate-300 pb-15 pt-30  md:px-20 " >
-          <h1 className="text-center mb-10 text-4xl bg-linear-90 md:mb-20 from-[#3B82F6] to-[#9333EA] bg-clip-text text-transparent font-bold">Generate Mikrotik Firewall Script</h1>
+          <h1 className="text-center mb-10 text-4xl bg-linear-90 md:mb-20 from-[#3B82F6] to-[#9333EA] bg-clip-text text-transparent font-bold">{lang === "en" ? "Generate Mikrotik Firewall Script" : "Membuat Script Firewall Mikrotik"}</h1>
 
           <div className="flex justify-center items-center flex-col gap-5">
             <div className="w-[85%]  h-50 md:h-[calc(100vh-150px)]">
               <form action="" className="flex gap-8 flex-col justify-center items-center bg-linear-90 from-[#3B82F6] to-[#9333EA] bg-clip-text text-transparent text-lg " onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col gap-2">
-                <label htmlFor="" className="font-bold">Input ASN Number :</label>
+                <label htmlFor="" className="font-bold">{lang === "en" ? "Input ASN Number :" : "Masukan nomor ASN"}</label>
                 <input type="text" placeholder="......." className="text-black py-0.5 px-2 bg-slate-100  font-semibold w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-400 focus:outline-none"
                 {...register("asn",{required : "Cannot Empty",pattern:{value : /^\d+$/, message:"ASN must a number"}})} />
                     {errors.asn && (<p className="text-red-600 md:-mt-1 md:-mb-2 font-semibold">{errors.asn.message}</p>)}
                 </div>
                 <div className="flex flex-col gap-2">
 
-                <label htmlFor="" className="font-bold">Address list name :</label>
+                <label htmlFor="" className="font-bold">{lang === "en" ? "Address list name :" : "Nama Address List :" }</label>
                 <input type="text" placeholder="......." className="text-black py-0.5 px-2 bg-slate-100  font-semibold w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-400 focus:outline-none"
                 {...register("addresListName", {required:{value:true,message: "Cannot Empty "} , minLength:{value:4 , message:"Minimal 4 Character"}})}
                 />
@@ -159,7 +159,7 @@ function App() {
                 </div>
                 <div className="flex flex-col gap-2">
 
-                <label htmlFor="" className="font-bold">File Name :</label>
+                <label htmlFor="" className="font-bold">{lang === "en" ? "File Name :" : "Nama File :"}</label>
                 <input type="text" placeholder="......." className="text-black py-0.5 px-2 bg-slate-100  font-semibold w-full border border-gray-300 rounded-lg p-2 focus:ring-2 focus:ring-blue-400 focus:outline-none"
                 {...register("fileName",{required:{value:true , message:"Cannot Empty" }, minLength:{value : 4 , message : "Minimal 4 Character"}})}
                 />
@@ -201,68 +201,65 @@ function App() {
         <section className="min-h-screen w-screen bg-slate-800 py-16  lg:px-20 ">
           
             <div className=" mx-auto ">
-                <h1 className="text-4xl text-center font-bold bg-linear-90 from-sky-400 to-purple-500 bg-clip-text text-transparent ">How to Block Facebook ASN on Mikrotik</h1>
-            
-
-            <div className="text-white font-semibold  mt-20 lg:text-xl ">
-              <ol className="list-decimal list-outside pl-[2rem] sm:max-w-[80%] md:max-w-screen space-y-10 mx-auto  ">
+                  <h1 className="text-4xl text-center font-bold bg-linear-90 from-sky-400 to-purple-500 bg-clip-text text-transparent ">{lang === "en" ? "How to Block Facebook ASN on MikroTik" : "Cara Memblokir ASN Facebook pada MikroTik"}</h1>
+            <div className="text-white font-semibold  mt-20 text-sm ">
+              <ol className="list-decimal list-outside px-[2rem] sm:max-w-[80%] md:max-w-screen space-y-10 mx-auto  ">
                 <li className="">
-                  Get the ASN number from<a target="_blank" href="https://bgpview.io/" className="font-bold"> bgpview.io</a>
+                    <p>{lang === "en" ?  "Get the ASN number from" : "Dapatkan nomor ASN dari"}<a target="_blank" href="https://bgpview.io/" className="font-bold"> bgpview.io</a></p>
                   <div className="flex flex-col md:flex-row ">
                   <img src="/tutorial/1.png" className="w-100 lg:w-130 lg:h-70 mt-2 ml-4" alt="" />
                   <img src="/tutorial/2.png" className="w-100 lg:w-130 lg:h-70 mt-2 ml-4" alt="" />
                   </div>
                 </li>
-                <li className="mt-10">
-                  Fill the form with ASN Number, Address List name (decide naming of address-list in MikroTik), and File Name (decide the file name for downloaded)
+                <li className="mt-20">
+                  {lang === "en" ? "Fill the form with ASN Number, Address List name (decide naming of address-list in MikroTik), and File Name (decide the file name for downloaded)" : "Isi form dengan nomor ASN, Nama Address List (untuk penamaan address-list di MikroTik), dan Nama File (untuk penamaan nama file yang akan diunduh)"
+                  }
                   <div className="flex flex-col md:flex-row">
                   <img src="/tutorial/3.png" className="w-100 lg:w-130 lg:h-90 mt-2 ml-4" alt="" />
                   <img src="/tutorial/5.png" className="w-100 lg:w-100 lg:h-90 mt-2 ml-4 md:ml-20" alt="" />
                   
                   </div>
                 </li>
-                <li className="mt-10">
-                  Download the file
+                <li className="mt-20">
+                  {lang === "en" ? "Download the file" : "Unduh file nya"}
                   <div className="flex flex-col md:flex-row">
                   <img src="/tutorial/4.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
                   <img src="/tutorial/6.png" className="w-100 lg:w-130 h-20 lg:h-30 mt-2 ml-4" alt="" />
                   </div>
                 </li>
-                <li className="mt-10">
-                  Drag the file into Files menu in MikroTik
+                <li className="mt-20">
+                  {lang === "en" ? "Drag the file into Files menu in MikroTik": "Seret/tarik file ke menu Files di MikroTik"
+                  }
                   <div className="flex flex-col md:flex-row">
                   <img src="/tutorial/7.png" className="w-100 lg:w-130 lg:h-90 mt-2 ml-4" alt="" />
                   <img src="/tutorial/8.png" className="w-100 lg:w-130 h-40 lg:h-50 mt-2 ml-4" alt="" />
-
-
                   </div>
                 </li>
-                <li className="mt-10">
-                  Write "<span className="text-[#CC66FF]">import</span> <span className="text-[#66FF66]">file-name</span><span className="text-amber-300">=</span>facebook.rsc" in terminal
+                <li className="mt-20">
+                    <p>{lang === "en" ? "Write command " : "Tulis perintah "}"<span className="text-[#CC66FF]">import</span> <span className="text-[#66FF66]">file-name</span><span className="text-amber-300">=</span>facebook.rsc" {lang === "en" ? "in terminal" : "di terminal"}</p> 
                   <div className="flex flex-col md:flex-row">
                   <img src="/tutorial/9.png" className="w-100 lg:w-130 md:h-10 mt-2 ml-4" alt="" />
                   <img src="/tutorial/10.png" className="w-100 lg:w-130 lg:h-60 mt-2 ml-4" alt="" />
-
-
                   </div>
                 </li>
-                <li className="mt-10">
-                  Check Address List
+                <li className="mt-20">
+                  {lang === "en" ? "Check Address List" : "Cek Address List"}
                   <div className="flex flex-col md:flex-row">
                   <img src="/tutorial/11.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
                   <img src="/tutorial/11.1.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
                   </div>
                 </li>
-                <li className="mt-10">
-                  Create a firewall rule. Set In/Out Interface to the interface that is connected to the network. Do the same thing with ipv6
+                <li className="mt-20">
+                  {lang === "en" ? "Create a firewall rule. Set In/Out Interface to the interface that is connected to the network, do the same thing with ipv6":"Buat sebuah rule firewall. Atur In/Out Interface dengan interface yang terkoneksi dengan jaringan internet, lakukan hal yang sama untuk ipv6"
+                  }
                   <div className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap">
-                  <img src="/tutorial/12.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/13.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/14.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/12.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/13.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/14.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 ml-4" alt="" />
                   </div>
                 </li>
-                <li className="mt-10">
-                  Result
+                <li className="mt-20">
+                  {lang === "en" ? "Result" : "Hasil"}
                   <div className="flex flex-col md:flex-row">
                   <img src="/tutorial/16.png" className="w-100 lg:w-130 md:h-10 mt-2 ml-4" alt="" />
                   <img src="/tutorial/15.png" className="w-100 lg:w-130 lg:h-80 mt-1 ml-4" alt="" />
@@ -275,6 +272,10 @@ function App() {
 
 
           </div>
+        </section>
+
+        <section className="w-screen h-[4rem] bg-amber-900 flex items-center justify-center">
+            <p className="text-slate-300">eilinseth&copy; 2025</p>
         </section>
     </div>
   )
